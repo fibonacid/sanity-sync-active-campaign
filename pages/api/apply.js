6b2eb7a32f7888ffd6cac7287b155cf67ff56499
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-03-28 16:45:07 +0200

    create person document

--- pages/api/apply.js
+++ pages/api/apply.js
@@ -1,5 +1,14 @@
-// pages/api/apply.js
+import { client } from "../../lib/sanity";
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
+  const doc = {
+    _type: "person",
+    firstName: "",
+    lastName: "",
+    email: "",
+    files: [],
+  };
+  const person = await client.create(doc);
+  console.log("Person created, id: ", person?._id);
   res.status(200).send("Thank you!");
 }
