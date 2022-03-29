32186b7e864957b9cad3ac59fa8b0b675f4e6767
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-03-29 13:26:35 +0200

    parse request body using multiparty

--- pages/api/apply.js
+++ pages/api/apply.js
@@ -1,6 +1,19 @@
 import { client } from "../../lib/sanity";
+import nc from "next-connect";
+import { formDataMiddleware } from "../../lib/middleware";
 
-export default async function handler(req, res) {
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const handler = nc();
+handler.use(formDataMiddleware);
+
+handler.post(async function handler(req, res) {
+  console.log(req.body);
+  console.log(req.files);
   const doc = {
     _type: "person",
     firstName: "",
@@ -11,4 +24,6 @@ export default async function handler(req, res) {
   const person = await client.create(doc);
   console.log("Person created, id: ", person?._id);
   res.status(200).send("Thank you!");
-}
+});
+
+export default handler;
--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,19 @@ export default function Home() {
       <form action="/api/apply" method="post" encType="multipart/form-data">
         <div className="field">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" required></input>
+          <input name="email" id="email" type="email" required></input>
         </div>
         <div className="field">
           <label htmlFor="firstName">First name</label>
-          <input id="firstName" required></input>
+          <input name="firstName" id="firstName" required></input>
         </div>
         <div className="field">
           <label htmlFor="lastName">Last name</label>
-          <input id="lastName" required></input>
+          <input name="lastName" id="lastName" required></input>
         </div>
         <div className="field">
           <label htmlFor="files">Documents (PDF)</label>
-          <input id="files" type="file" multiple required></input>
+          <input name="files" id="files" type="file" multiple required></input>
         </div>
         <button type="submit">Send</button>
       </form>
