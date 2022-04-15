773f6e4c1c798531cd633a26616d0833e4d279ff
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-04-15 15:36:09 +0200

    upload assets to sanity

--- pages/api/apply.js
+++ pages/api/apply.js
@@ -1,6 +1,7 @@
 import { client } from "../../lib/sanity";
 import nc from "next-connect";
 import { formDataMiddleware } from "../../lib/middleware";
+import { readFile } from "fs/promises";
 
 export const config = {
   api: {
@@ -11,6 +12,13 @@ export const config = {
 
 const handler = nc();
 handler.use(formDataMiddleware);
+
+async function uploadFile(file) {
+  const blob = await readFile(file?.path);
+  return client.assets.upload("file", blob, {
+    filename: file?.originalFilename || undefined,
+  });
+}
 
 handler.post(async function handler(req, res) {
   const { body, files } = req;
@@ -27,6 +35,29 @@ handler.post(async function handler(req, res) {
     email,
     files: [],
   };
+
+  doc.files = files?.files?.map(async (file) => {
+    // Read file and store as a buffer
+    const buffer = await readFile(file?.path);
+    // Upload buffer to sanity
+    const asset = await client.assets.upload("file", buffer, {
+      filename: file?.originalFilename || undefined,
+    });
+    console.log("File uploaded, id:", asset?._id);
+    // Reference asset inside the document
+    const reference = {
+      _type: "file",
+      asset: {
+        _type: "reference",
+        _ref: asset?._id || null,
+      },
+    };
+    return reference;
+  });
+
+  // Wait until all file promises have completed
+  await Promise.all(doc.files);
+
   const person = await client.create(doc);
   console.log("Person created, id: ", person?._id);
 
